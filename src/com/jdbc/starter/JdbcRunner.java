158b56ca2bc987b600c44e328e4cffe7d4e0598d
kerimovikh <kerimovikh@mail.ru>
2022-04-05 21:17:59 +0300

    first commit in master branch

---
+++ src/com/jdbc/starter/JdbcRunner.java
@@ -0,0 +1,18 @@
+package com.jdbc.starter;
+
+import com.jdbc.starter.util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcRunner {
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+
+        try (Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+    }
+}
