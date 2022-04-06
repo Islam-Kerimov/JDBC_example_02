83f50860d557048e0c9402548e14166a0469c7da
kerimovikh <kerimovikh@mail.ru>
2022-04-06 14:21:41 +0300

    metadata

--- src/com/jdbc/starter/JdbcRunner.java
+++ src/com/jdbc/starter/JdbcRunner.java
@@ -15,10 +15,35 @@ public class JdbcRunner {
 //        List<Long> result = getTicketsByFlightId(flightId);
 //        System.out.println(result);
 
-        List<Long> result = getFlightBetween(
-                LocalDate.of(2020, 1, 1).atStartOfDay(),
-                LocalDateTime.now());
-        System.out.println(result);
+//        List<Long> result = getFlightBetween(
+//                LocalDate.of(2020, 1, 1).atStartOfDay(),
+//                LocalDateTime.now());
+//        System.out.println(result);
+
+        checkMetaData();
+    }
+
+    private static void checkMetaData() throws SQLException {
+        try (Connection connection = ConnectionManager.open()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet catalogs = metaData.getCatalogs();
+            while (catalogs.next()) {
+                String catalog = catalogs.getString(1);
+
+                ResultSet schemas = metaData.getSchemas();
+                while (schemas.next()) {
+                    String schema = schemas.getString("TABLE_SCHEM");
+
+                    ResultSet tables = metaData.getTables(catalog, schema, "%", new String[] {"TABLE"});
+                    if (schema.equals("public")) {
+                        while (tables.next()) {
+                            System.out.println(tables.getString("TABLE_NAME"));
+                        }
+                    }
+                }
+            }
+
+        }
     }
 
     private static List<Long> getFlightBetween(LocalDateTime start, LocalDateTime end) {
