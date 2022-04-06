1c1a7d4716698d29e6d511fad2f23429e9bf6176
kerimovikh <kerimovikh@mail.ru>
2022-04-06 12:39:22 +0300

    example SQL injection

--- src/com/jdbc/starter/JdbcRunner.java
+++ src/com/jdbc/starter/JdbcRunner.java
@@ -4,25 +4,32 @@ import com.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
-        Class<Driver> driverClass = Driver.class;
+        String flightId = "2 OR '' = '' ";
+        List<Long> result = getTicketsByFlightId(flightId);
+        System.out.println(result);
+    }
 
-        String sql = "INSERT INTO info(data) VALUES ('fsdfsdfs')";
+    private static List<Long> getTicketsByFlightId(String flightId) {
+        String sql = "SELECT id FROM ticket WHERE flight_id = %s".formatted(flightId);
+
+        List<Long> result = new ArrayList<>();
+
         try (Connection connection = ConnectionManager.open();
-             Statement statement = connection.createStatement(
-                     ResultSet.TYPE_SCROLL_INSENSITIVE,
-                     ResultSet.CONCUR_UPDATABLE)) {
-//            System.out.println(connection.getSchema());
-//            System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-            ResultSet generatedKeys = statement.getGeneratedKeys();
+             Statement statement = connection.createStatement()) {
 
-            if (generatedKeys.next()) {
-                int generatedKeysInt = generatedKeys.getInt("id");
-                System.out.println(generatedKeysInt);
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                result.add(resultSet.getObject("id", Long.class));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+        return result;
     }
 }
